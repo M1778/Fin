67808ebf95069a0285b48bdd8755e808f73ab9a3
M1778M <m1778.pc@gmail.com>
2025-12-29 07:43:33 +0330

    Minor changes

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class FinConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     requires = (
-        "llvm-core/19.1.7",
+        #"llvm-core/19.1.7", -> DO NOT USE FOR LINUX
         "fmt/10.2.1",
         "gtest/1.14.0",
     )
