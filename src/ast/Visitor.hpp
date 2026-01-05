d261c505c196afb93bffb334589ae8f625be2d14
M1778 <m1778.pc@gmail.com>
2026-01-05 10:47:30 +0330

    visitor got better, handles more nodes now

--- src/ast/Visitor.hpp
+++ src/ast/Visitor.hpp
@@ -12,6 +12,9 @@ class EnumDeclaration;
 class DefineDeclaration;
 class MacroDeclaration;
 class OperatorDeclaration;
+class ImportModule;
+class ConstructorDeclaration;
+class DestructorDeclaration;
 
 class Block;
 class ReturnStatement;
@@ -25,22 +28,34 @@ class ContinueStatement;
 class DeleteStatement;
 class TryCatch;
 class BlameStatement;
-class ImportModule; // Forward declaration
-class MethodCall;
-
 
 class BinaryOp;
 class UnaryOp;
 class Literal;
 class Identifier;
 class FunctionCall;
+class MethodCall;
 class MacroCall;
+class MacroInvocation;
+class QuoteExpression;
 class CastExpression;
 class NewExpression;
 class MemberAccess;
 class StructInstantiation;
 class ArrayLiteral;
+class ArrayAccess;
 class SizeofExpression;
+class TernaryOp;
+class FunctionTypeNode;
+class LambdaExpression;
+class TypeNode;
+class SuperExpression;
+class PointerTypeNode;
+class ArrayTypeNode;
+
+// Missing nodes
+class Parameter;
+class StructMember;
 
 class Visitor {
 public:
@@ -58,7 +73,13 @@ public:
     virtual void visit(DefineDeclaration& node) = 0;
     virtual void visit(MacroDeclaration& node) = 0;
     virtual void visit(OperatorDeclaration& node) = 0;
-    virtual void visit(ImportModule& node) = 0; // Added missing method
+    virtual void visit(ImportModule& node) = 0;
+    virtual void visit(ConstructorDeclaration& node) = 0;
+    virtual void visit(DestructorDeclaration& node) = 0;
+    
+    // Helpers
+    virtual void visit(Parameter& node) = 0;      // <--- ADDED
+    virtual void visit(StructMember& node) = 0;   // <--- ADDED
 
     // Statements
     virtual void visit(Block& node) = 0;
@@ -73,7 +94,6 @@ public:
     virtual void visit(DeleteStatement& node) = 0;
     virtual void visit(TryCatch& node) = 0;
     virtual void visit(BlameStatement& node) = 0;
-    virtual void visit(MethodCall& node) = 0;
 
     // Expressions
     virtual void visit(BinaryOp& node) = 0;
@@ -81,13 +101,24 @@ public:
     virtual void visit(Literal& node) = 0;
     virtual void visit(Identifier& node) = 0;
     virtual void visit(FunctionCall& node) = 0;
+    virtual void visit(MethodCall& node) = 0;
     virtual void visit(MacroCall& node) = 0;
+    virtual void visit(MacroInvocation& node) = 0;
+    virtual void visit(QuoteExpression& node) = 0;
     virtual void visit(CastExpression& node) = 0;
     virtual void visit(NewExpression& node) = 0;
     virtual void visit(MemberAccess& node) = 0;
     virtual void visit(StructInstantiation& node) = 0;
     virtual void visit(ArrayLiteral& node) = 0;
+    virtual void visit(ArrayAccess& node) = 0;
     virtual void visit(SizeofExpression& node) = 0;
+    virtual void visit(TernaryOp& node) = 0;
+    virtual void visit(FunctionTypeNode& node) = 0;
+    virtual void visit(LambdaExpression& node) = 0;
+    virtual void visit(TypeNode& node) = 0;
+    virtual void visit(SuperExpression& node) = 0;
+    virtual void visit(PointerTypeNode& node) = 0;
+    virtual void visit(ArrayTypeNode& node) = 0;
 };
 
 } // namespace fin
