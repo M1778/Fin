b1330028babedd2098eda7204cf5a3774094c073
M1778 <m1778.pc@gmail.com>
2026-01-05 10:53:43 +0330

    Scope refined

--- src/semantics/Scope.hpp
+++ src/semantics/Scope.hpp
@@ -2,7 +2,10 @@
 #include <string>
 #include <unordered_map>
 #include <memory>
-#include "Type.hpp"
+#include "../types/Type.hpp"
+
+// Forward decl
+namespace fin { class MacroDeclaration; }
 
 namespace fin {
 
@@ -18,11 +21,15 @@ public:
     Scope* parent;
     std::unordered_map<std::string, Symbol> symbols;
     std::unordered_map<std::string, std::shared_ptr<Type>> types;
+    
+    std::unordered_map<std::string, MacroDeclaration*> macros;
 
     Scope(Scope* p = nullptr) : parent(p) {}
 
     void define(Symbol sym) { symbols[sym.name] = sym; }
     void defineType(std::string name, std::shared_ptr<Type> type) { types[name] = type; }
+    
+    void defineMacro(std::string name, MacroDeclaration* macro) { macros[name] = macro; }
 
     Symbol* resolve(const std::string& name) {
         if (symbols.count(name)) return &symbols[name];
@@ -33,6 +40,12 @@ public:
     std::shared_ptr<Type> resolveType(const std::string& name) {
         if (types.count(name)) return types[name];
         if (parent) return parent->resolveType(name);
+        return nullptr;
+    }
+    
+    MacroDeclaration* resolveMacro(const std::string& name) {
+        if (macros.count(name)) return macros[name];
+        if (parent) return parent->resolveMacro(name);
         return nullptr;
     }
 };
